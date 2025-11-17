9d275c93d06ad2551b5bdfef663b3ffa978bf333
Jossue Toscano <alejandrio.12@hotmail.com>
2025-11-16 22:23:05 -0500

    tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill para scrollIntoView en el entorno de testing
+if (typeof window !== 'undefined' && window.HTMLElement && !window.HTMLElement.prototype.scrollIntoView) {
+	window.HTMLElement.prototype.scrollIntoView = function() {};
+}
