f75eec6f6f834c76c87c81a7fa90af649220d9ae
Jossue Toscano <alejandrio.12@hotmail.com>
2025-11-16 22:21:31 -0500

    otros

--- src/__tests__/AdminLogin.test.js
+++ src/__tests__/AdminLogin.test.js
@@ -6,16 +6,16 @@ import axios from 'axios';
 jest.mock('axios');
 
 test('realiza login y llama onLoginSuccess', async () => {
-  const token = 'fake.token';
-  axios.post.mockResolvedValueOnce({ data: { token } });
+    const token = 'fake.token';
+    axios.post.mockResolvedValueOnce({ data: { token } });
 
-  const onLoginSuccess = jest.fn();
-  const onBack = jest.fn();
+    const onLoginSuccess = jest.fn();
+    const onBack = jest.fn();
 
-  render(<AdminLogin onLoginSuccess={onLoginSuccess} onBack={onBack} />);
+    render(<AdminLogin onLoginSuccess={onLoginSuccess} onBack={onBack} />);
 
-  const button = screen.getByRole('button', { name: /Acceder/i });
-  fireEvent.click(button);
+    const button = screen.getByRole('button', { name: /Acceder/i });
+    fireEvent.click(button);
 
-  await waitFor(() => expect(onLoginSuccess).toHaveBeenCalledWith(token, 'admin'));
+    await waitFor(() => expect(onLoginSuccess).toHaveBeenCalledWith(token, 'admin'));
 });
